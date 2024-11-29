d6b7027b5adb4e09565a7b5342bb32e741127192
GustavoGomes-dev <gustavo10.ggj@gmail.com>
2024-11-29 16:53:27 -0300

    Update JogosController.java

--- controller/JogosController.java
+++ controller/JogosController.java
@@ -1,83 +1,50 @@
-package catalago.de.jogos.demo.controller;
+package com.example.demo.Controller;
 
-import java.net.URI;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import catalago.de.jogos.demo.dto.JogosDto;
-import catalago.de.jogos.demo.dto.UpdateJogoDto;
-import catalago.de.jogos.demo.model.JogosModel;
-import catalago.de.jogos.demo.service.JogosService;
+import com.example.demo.dto.UpdateJogoDto;
+import com.example.demo.model.JogosModel;
+import com.example.demo.service.JogosService;
 
 @RestController
-@CrossOrigin("*")
-@RequestMapping ("Jogos")
+@RequestMapping("/catalogo")
 public class JogosController {
-	
-	private JogosService jogosService;
 
+    @Autowired
+    private JogosService jogosService;
 
-	public JogosController(JogosService jogosService) {
-		
-		this.jogosService = jogosService;
-	}
+    @PostMapping("/adicionar")
+    public ResponseEntity<JogosModel> adicionarCatalogo(@RequestBody JogosModel jogo) {
+        JogosModel jogoCriado = jogosService.criarJogo(jogo);
+        return new ResponseEntity<>(jogoCriado, HttpStatus.CREATED);
+    }
 
+    @GetMapping("/listar")
+    public ResponseEntity<List<JogosModel>> listarCatalogo() {
+        List<JogosModel> jogos = jogosService.listarJogos();
+        return ResponseEntity.ok(jogos);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<JogosModel> getJogoPorId(@PathVariable("id") Long id) {
+        return jogosService.getJogoPorId(id)
+                .map(jogo -> ResponseEntity.ok(jogo))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<JogosModel> atualizarJogo(@PathVariable("id") Long id, @RequestBody JogosModel jogoAtualizado) {
+        JogosModel jogo = jogosService.atualizarJogo(id, jogoAtualizado);
+        return jogo != null ? ResponseEntity.ok(jogo) : ResponseEntity.notFound().build();
+    }
 
-
-
-	
-	@PostMapping
-	public ResponseEntity<JogosModel> createJogos(@RequestBody JogosDto jogosDto) {
-	    var jogoid = jogosService.createJogos(jogosDto);
-	    return ResponseEntity.created(URI.create("/Jogos/" + jogoid.toString())).build(); 
-	}
-
-	
-	@GetMapping("/{userId}")
-	public ResponseEntity<JogosModel> getUserById(@PathVariable("userId") String userId){
-		var jogo = jogosService.getUserById(userId);
-		if(jogo.isPresent()) {
-			return ResponseEntity.ok(jogo.get());
-		}else {
-			return ResponseEntity.notFound().build();
-		}
-		
-	}
-	
-	@GetMapping
-	public ResponseEntity<List<JogosModel>> listJogos(){
-		var jogos = jogosService.listJogos();
-		
-		return ResponseEntity.ok(jogos);
-		
-	}
-	
-	@PutMapping("/{userId}")
-	public ResponseEntity<Void> updateUserById(@PathVariable("userId") String userId, @RequestBody UpdateJogoDto upadateJogoDto){
-		jogosService.updateJogoById(userId, upadateJogoDto);
-		return ResponseEntity.noContent().build();
-		
-		
-		
-	}
-	
-	@DeleteMapping("/{userId}")
-	public ResponseEntity<Void> deleteById(@PathVariable("userId") String userId){
-		jogosService.deleteById(userId);
-		return ResponseEntity.noContent().build();
-		
-		
-	}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarJogo(@PathVariable("id") Long id) {
+        return jogosService.deletarJogo(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+    }
 }
